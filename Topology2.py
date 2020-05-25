6315f3a16c280c32688d9f03aa0664645fface7a
Bhushan Hiwase <bhushiwase@gmail.com>
2020-05-24 17:51:31 -0700

    Add files via upload

---
+++ Topology2.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=Controller,
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
+    s5 = net.addSwitch('s5', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s6 = net.addSwitch('s6', cls=OVSKernelSwitch)
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
+
+    info( '*** Add hosts\n')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+    h7 = net.addHost('h7', cls=Host, ip='10.0.0.7', defaultRoute=None)
+    h8 = net.addHost('h8', cls=Host, ip='10.0.0.8', defaultRoute=None)
+    h5 = net.addHost('h5', cls=Host, ip='10.0.0.5', defaultRoute=None)
+    h10 = net.addHost('h10', cls=Host, ip='10.0.0.10', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
+    h6 = net.addHost('h6', cls=Host, ip='10.0.0.6', defaultRoute=None)
+    h11 = net.addHost('h11', cls=Host, ip='10.0.0.11', defaultRoute=None)
+    h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
+    h12 = net.addHost('h12', cls=Host, ip='10.0.0.12', defaultRoute=None)
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h9 = net.addHost('h9', cls=Host, ip='10.0.0.9', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(s6, s1)
+    net.addLink(s6, s2)
+    net.addLink(s3, s5)
+    net.addLink(s5, s4)
+    net.addLink(s6, s5)
+    net.addLink(s1, h1)
+    net.addLink(s1, h2)
+    net.addLink(s1, h3)
+    net.addLink(s2, h4)
+    net.addLink(s2, h5)
+    net.addLink(s2, h6)
+    net.addLink(s3, h7)
+    net.addLink(s3, h8)
+    net.addLink(s3, h9)
+    net.addLink(s4, h10)
+    net.addLink(s4, h11)
+    net.addLink(s4, h12)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s3').start([])
+    net.get('s5').start([c0])
+    net.get('s2').start([])
+    net.get('s1').start([])
+    net.get('s6').start([c0])
+    net.get('s4').start([])
+
+    info( '*** Post configure switches and hosts\n')
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
